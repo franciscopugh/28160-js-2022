60ea31eab88773941c261aeda3416f90e2b28563
Francisco Pugh <franciscopugh01@gmail.com>
2022-01-27 13:29:33 -0300

    Clase 1

---
+++ clase-1/script.js
@@ -0,0 +1,44 @@
+// Esto es un comentario
+
+/*
+    Esto es otro comentario que dice que este codigo es de la primer clase de JavaScript
+*/
+/*
+nombre = "Francisco"
+var edad = 40
+
+let nombre1 = "Pepita"  //String
+nombre = "Pepe"
+nombre1 = "Juan"
+
+nombre1 = 5; //Number
+
+/*
+console.log(nombre)
+
+nombre = "Pepe"
+
+console.log(nombre)
+console.log(edad)
+
+console.log(nombre1)*/
+
+const IVA = 1.21
+const IMPUESTO_PAIS = 1.30
+const RETENCION_GANANCIAS = 1.35
+
+let dolar = Number(prompt("Ingrese el valor del dolar"))
+
+let numeroUno = 10
+let numero2 = "5"
+numero1 = 15;
+
+const dolarSolidario = dolar + IMPUESTO_PAIS + RETENCION_GANANCIAS
+console.log(`El valor del Dolar solidario es ${dolarSolidario}`)
+document.write("<h1>dolarSolidario</h1>")
+document.write(`<h1>${dolarSolidario}</h1>`)
+document.write("<h1>" +  dolarSolidario + "</h1>")
+/*alert(`El valor del Dolar solidario es ${dolarSolidario}`)
+alert(`El valor del Dolar solidario es ${dolarSolidario}`)
+alert(`El valor del Dolar solidario es ${dolarSolidario}`)
+alert(`El valor del Dolar solidario es ${dolarSolidario}`)*/
