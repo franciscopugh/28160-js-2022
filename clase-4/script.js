eacf2035dc63436b6674d1d1a0d71a6eb679cfe7
Francisco Pugh <franciscopugh01@gmail.com>
2022-02-02 22:43:05 -0300

    Clase 4

---
+++ clase-4/script.js
@@ -0,0 +1,54 @@
+/*
+function mostrarNombre(nombre) {
+    console.log(`El profe es ${nombre}`)
+}
+
+mostrarNombre(1)
+mostrarNombre(true)
+mostrarNombre(null)
+mostrarNombre("Dibu")
+mostrarNombre("Messi")
+let acumuladoSuma = 0;
+
+function suma(num1, num2) {
+    return num1 + num2
+}
+
+let numero1 = parseFloat(prompt("Ingrese un numero"))
+let numero2 = parseFloat(prompt("Ingrese otro numero"))
+
+console.log(suma(numero1, numero2))
+acumuladoSuma += suma(numero1,numero2)
+acumuladoSuma += 50
+
+//const IVA = 1.21
+const IVA1 = 1.22
+function calculoIVA(producto) {
+    const IVA2 = -1.21
+    console.log(IVA2)
+    return producto * IVA2
+}
+
+console.log(calculoIVA(100))
+console.log(IVA1)
+
+function suma(num1, num2) {
+    return num1 + num2
+}
+
+const suma = function(num1, num2) {return num1 + num2}
+const resta = function(num1, num2) {return num1 - num2}
+const division = function(num1, num2) {return num1 / num2}
+const multiplicacion = function(num1, num2) {return num1 * num2}
+*/
+
+
+const suma = (num1, num2) => num1 + num2
+const resta = (num1, num2) => num1 - num2
+const division = (num1, num2) => num1 / num2
+const multiplicacion = (num1, num2) => num1 * num2
+
+console.log(suma(5,10))
+console.log(resta(5,10))
+console.log(division(5,10))
+console.log(multiplicacion(5,10))
